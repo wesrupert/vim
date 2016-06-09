5812eca93bc376fe28511352ec65a96e384d4112
Wes Rupert <wesr@microsoft.com>
2016-06-09 16:20:26 -0700

    Make subdirectories as necessary

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git submodule update --init
-mkdir ~/.vim/autoload
+mkdir -p ~/.vim/autoload
 ln -f ~/.vim/vimrc ~/.vimrc
 ln -f ~/.vim/bundle/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
